8b5d4f50b7503fc32ee8b4a01115643d55dd374a
David N. Welton <davidw@dedasys.com>
2011-02-15 13:39:48 +0100

    Added Alex Ostleitner's refactoring.

--- lib/google_screener.rb
+++ lib/google_screener.rb
@@ -2,11 +2,41 @@
 
 require 'open-uri'
 require 'json'
+require 'cgi'
 
 # This is a Ruby interface to the JSON results provided by the Google
 # Screener.  It's not an official API.
 
 class GoogleScreener
+
+  BASE_URI = "http://www.google.com/finance?"
+  BASE_PARAMS = {
+    :gl       => "us",
+    :hl       => "en",
+    :output   => "json",
+    :restype  => "company",
+    :sortas   => "Price52WeekPercChange",
+    :desc     => 1,
+    :noIL     => 1,
+    :start    => 0,
+    :num      => 20
+  }
+  SCREENER_MIN = {
+    :MarketCap                  => 10_000_000,
+    :PE                         => 0,
+    :DividendYield              => 0.01,
+    :Price52WeekPercChange      => -97.77,
+    :NetIncomeGrowthRate5Years  => -78.78,
+    :QuoteLast                  => 0.03
+  }
+  SCREENER_MAX = {
+    :MarketCap                  => 1_500_000_000_000,
+    :PE                         => 100,
+    :DividendYield              => 38.03,
+    :Price52WeekPercChange      => -15,
+    :NetIncomeGrowthRate5Years  => 419,
+    :QuoteLast                  => 122811
+  }
 
   class Result
     attr_reader :name, :symbol, :values
@@ -19,36 +49,37 @@ class GoogleScreener
 
   attr_reader :results
 
-  def initialize()
-    # This query isn't actually used as-is; the 'q' gets replaced.
-    @fullquery = [["gl", "us"], ["hl", "en"], ["output", "json"], ["start", "0"], ["num", "20"], ["noIL", "1"], ["q", "((exchange:NYSE) OR (exchange:NASDAQ) OR (exchange:AMEX)) [(MarketCap > 10000000 | MarketCap = 10000000) & (MarketCap < 1500000000000 | MarketCap = 1500000000000) & (PE > 0 | PE = 0) & (PE < 100 | PE = 100) & (DividendYield > 0.01 | DividendYield = 0.01) & (DividendYield < 38.03 | DividendYield = 38.03) & (Price52WeekPercChange > -97.77 | Price52WeekPercChange = -97.77) & (Price52WeekPercChange < -15 | Price52WeekPercChange = -15) & (TotalDebtToAssetsYear > 0 | TotalDebtToAssetsYear = 0) & (TotalDebtToAssetsYear < 0 | TotalDebtToAssetsYear = 0) & (NetIncomeGrowthRate5Years > -78.78 | NetIncomeGrowthRate5Years = -78.78) & (NetIncomeGrowthRate5Years < 419 | NetIncomeGrowthRate5Years = 419) & (QuoteLast > 0.03 | QuoteLast = 0.03) & (QuoteLast < 122811 | QuoteLast = 122811)]"], ["restype", "company"], ["sortas", "Price52WeekPercChange"], ["desc", "1"]]
-    @url = "http://www.google.com/finance"
+  def initialize(options)
+    merged_minimums = SCREENER_MIN.merge(options[:min])
+    merged_maximums = SCREENER_MAX.merge(options[:max])
+    option_string   = merged_minimums.map { |key, value| "(#{CGI.escape(key.to_s)} > #{value.to_f})" }.join(" & ")
+    option_string  += " & "
+    option_string  += merged_maximums.map { |key, value| "(#{CGI.escape(key.to_s)} < #{value.to_f})" }.join(" & ")
+    query   = "((exchange:NYSE) OR (exchange:NASDAQ) OR (exchange:AMEX)) [#{option_string}]"
+    @params = BASE_PARAMS.merge(:q => query)
   end
 
-  def fetch(querytype)
-    query = make_query(querytype)
+  def sort_as(how)
+    @params.merge!(:sortas => how)
+    self
+  end
 
-    # FIXME: 1.9 can use this: url = @url + "?&" + URI.encode_www_form(query)
-    url = @url + "?&" + query.map { |x| "#{CGI.escape(x[0])}=#{CGI.escape(x[1])}" }.join("&")
-    @results = JSON.parse(open(url).read)['searchresults'].map { |res| Result.new(res) }
+  def fetch
+    @results = get_data.map { |res| Result.new(res) }
   end
 
   private
 
-  # QuotePercChange
+  def get_data
+    JSON.parse(open(uri).read)['searchresults']
+  end
 
-  def make_query(querytype)
-    query = "((exchange:NYSE) OR (exchange:NASDAQ) OR (exchange:AMEX)) [(MarketCap > 10000000 | MarketCap = 10000000) & (MarketCap < 1500000000000 | MarketCap = 1500000000000) & (PE > 0 | PE = 0) & (PE < 100 | PE = 100) & (DividendYield > 0.01 | DividendYield = 0.01) & (DividendYield < 38.03 | DividendYield = 38.03) & (#{querytype} > -200.00) & (#{querytype} < -15) & (TotalDebtToAssetsYear > 0 | TotalDebtToAssetsYear = 0) & (TotalDebtToAssetsYear < 0 | TotalDebtToAssetsYear = 0) & (NetIncomeGrowthRate5Years > -78.78 | NetIncomeGrowthRate5Years = -78.78) & (NetIncomeGrowthRate5Years < 419 | NetIncomeGrowthRate5Years = 419) & (QuoteLast > 0.03 | QuoteLast = 0.03) & (QuoteLast < 122811 | QuoteLast = 122811)]"
-    sortas = querytype
-    return @fullquery.map do |x|
-      if x[0] == "q"
-        ["q", query]
-      elsif x[0] == "sortas"
-        ["sortas", sortas] 
-      else
-        x
-      end
-    end
+  def uri
+    BASE_URI + query_string
+  end
+
+  def query_string
+    @params.map { |key, value| "#{CGI.escape(key.to_s)}=#{CGI.escape(value.to_s)}" }.join("&")
   end
 
 end
